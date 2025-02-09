b171289192c2456d47cc70efd86cbaec068856fe
vultorio <68811816+vultorio67@users.noreply.github.com>
2025-02-09 21:47:39 +0100

    add new servomoteur

--- projet-charette/projet-charette.ino
+++ projet-charette/projet-charette.ino
@@ -5,25 +5,27 @@
 Ultrasonic ultrasonicForward(9, 8);
 Ultrasonic ultrasonicRight(12, 13);
 
+//the wheels
 Servo rouedroite;
 Servo rouegauche;
+
+//captor
+Servo servoDirection;
 
 const int fowardThreshold = 7;
 const int rightThresholdMin = 3;
 const int rightThresholdMax = 5;
  
 int arret = 1500;
- 
-// variable vitesse et de direction pour le servomoteur 
-int temps = 0;
- 
-// Declaration de la variable pour la rampe de demarrage
-int valeur_de_vitesse_demarrage;
 
+//in case if needed
 unsigned long rampedemarrage = millis();
  
-int distanceRightSensor;
+int distanceLateralSensor;
 int distanceForwardSensor;
+
+////// à déterminer comment 
+bool isRight = true;
  
  
 void setup() {
@@ -48,8 +50,18 @@ void loop() {
   Serial.println(" cm");
 
   Serial.print("Distance right = ");
-  Serial.print(distanceRightSensor); // Distance will be 0 when out of set max range.
+  Serial.print(distanceLateralSensor); // Distance will be 0 when out of set max range.
   Serial.println(" cm");
+
+  if(isRight)
+  {
+    //à voir pour l'angle
+    servoDirection.write(180);
+  }
+  else 
+  {
+    servoDirection.write(0);
+  }
 
   delay(200);
 
@@ -58,19 +70,34 @@ void loop() {
 
 void condition()
 {
-  if(distanceForwardSensor >= fowardThreshold && distanceRightSensor <= rightThresholdMax && distanceRightSensor >= rightThresholdMin)
+  //if nothing foward and correct distance on side
+  if(distanceForwardSensor >= fowardThreshold && distanceLateralSensor <= rightThresholdMax && distanceLateralSensor >= rightThresholdMin)
   {
     forward();
   }
 
-  else if (distanceForwardSensor < fowardThreshold && distanceRightSensor <= rightThresholdMax)
+  //if wall in front and wall on side
+  else if (distanceForwardSensor < fowardThreshold && distanceLateralSensor <= rightThresholdMax)
   {
-    turnLeftSelf();
+    if(isRight)
+    {
+      turnLeftSelf();
+    }
+    else {
+      turnRightSelf();
+    }
   }
 
-  else if (distanceRightSensor >= rightThresholdMax)
+  //if lateral distance is too big
+  else if (distanceLateralSensor >= rightThresholdMax)
   {
-    turnRight();
+    if(isRight)
+    {
+      turnRight();
+    }
+    else{
+      turnLeft();
+    }
   }
   
   else
@@ -84,7 +111,7 @@ void readDisance()
 {
     distanceForwardSensor = ultrasonicForward.read(CM);
     delay(50);
-    distanceRightSensor = ultrasonicRight.read(CM);
+    distanceLateralSensor = ultrasonicRight.read(CM);
 
 }
 
@@ -94,13 +121,21 @@ void forward() {
   rouegauche.writeMicroseconds(1700);
 }
 
+//One wheel stoped
 void turnRight()
 {
   rouedroite.writeMicroseconds(1500);   
   rouegauche.writeMicroseconds(1300);
 }
 
+//One wheel stoped
+void turnLeft()
+{
+  rouedroite.writeMicroseconds(1700);   
+  rouegauche.writeMicroseconds(1500);
+}
 
+//turn on it self
 void turnRightSelf()
 {
   rouedroite.writeMicroseconds(1300);   
@@ -110,6 +145,7 @@ void turnRightSelf()
   delay(100);
 }
 
+//turn on it self
 void turnLeftSelf()
 {
   rouedroite.writeMicroseconds(1700);   
