39bd81164fbbf50b8fb24a2e40883f4efc9a73f8
vultorio <68811816+vultorio67@users.noreply.github.com>
2025-02-17 13:27:20 +0100

    Update projet-charette.ino

--- projet-charette/projet-charette.ino
+++ projet-charette/projet-charette.ino
@@ -15,15 +15,18 @@ Servo rouegauche;
 //captor
 Servo servoDirection;
 
-const int fowardThreshold = 8;
+const int forwardThreshold = 8;
 const int rightThresholdMin = 6;
 const int rightThresholdMax = 9;
-const int rightThresholdRotation = 15;
+const int rightThresholdRotation = 20;
+const int forwardgoBackAndTurnThreshOld = 85;
+
+const int maxSameMoveDelay = 1500;
  
 int arret = 1500;
 
 //in case if needed
-unsigned long rampedemarrage = millis();
+unsigned long startTime = millis();
  
 int distanceLateralSensor;
 int distanceForwardSensor;
@@ -32,6 +35,9 @@ int distanceForwardSensor;
 bool isRight = true;
 
 bool isStart;
+
+//0 = stop, 1 = forward, 2 = goBackAndTurn, 3 = turnRight, 4 = turnLeft
+int movingMode = 0;
  
  
 void setup() {
@@ -69,21 +75,22 @@ void loop() {
 
   printInfo();
 
-  delay(200);
+  delay(3);
 
 
 }
 
 void condition()
 {
-  //if nothing foward and correct distance on side
-  if(distanceForwardSensor >= fowardThreshold && distanceLateralSensor <= rightThresholdMax && distanceLateralSensor >= rightThresholdMin)
+  //if nothing forward and correct distance on side
+  if(distanceForwardSensor >= forwardThreshold && distanceLateralSensor <= rightThresholdMax && distanceLateralSensor >= rightThresholdMin)
   {
     forward();
+    Serial.println("avance");
   }
 
   //if wall in front and wall on side
-  else if (distanceForwardSensor < fowardThreshold && distanceLateralSensor <= rightThresholdMax)
+  else if (distanceForwardSensor < forwardThreshold && distanceLateralSensor <= rightThresholdMax)
   {
     if(isRight)
     {
@@ -95,7 +102,7 @@ void condition()
   }
 
   //if no wall detected
-  else if (distanceForwardSensor > fowardThreshold && distanceLateralSensor > rightThresholdMax)
+  else if (distanceForwardSensor > forwardThreshold && distanceLateralSensor > rightThresholdMax)
   {
     if(isRight)
     {
@@ -108,7 +115,7 @@ void condition()
   }
 
   //if wall in front and no wall on side
-  else if (distanceForwardSensor < fowardThreshold && distanceLateralSensor > rightThresholdRotation)
+  else if (distanceForwardSensor < forwardThreshold && distanceLateralSensor > rightThresholdRotation)
   {
     if(isRight)
     {
@@ -119,7 +126,6 @@ void condition()
       turnLeftSelf();
     }
   }
-
   //if too close to the side wall
   else if(distanceLateralSensor < rightThresholdMin)
   {
@@ -144,6 +150,14 @@ void condition()
       turnLeft();
     }
   }
+
+    else if (distanceForwardSensor > forwardgoBackAndTurnThreshOld)
+    {
+      goBackAndTurn();
+      delay(300);
+      stop();
+      delay(70);
+    }
   
   else
   {
@@ -161,48 +175,100 @@ void readDisance()
 }
 
 
+
 void forward() {
-  rouedroite.writeMicroseconds(1300);   
-  rouegauche.writeMicroseconds(1700);
+  if(movingMode != 1)
+  {
+    startTime = millis();
+  }
+  movingMode = 1;
+
+  if(millis() - startTime > maxSameMoveDelay)
+  {
+        goBackAndTurn();
+  }
+  else {
+    rouedroite.writeMicroseconds(1300);   
+    rouegauche.writeMicroseconds(1700);
+  }
 }
 
 //One wheel stoped
 void turnRight()
 {
-  rouedroite.writeMicroseconds(1475);   
-  rouegauche.writeMicroseconds(1700);
+
+  if(movingMode != 3)
+  {
+    startTime = millis();
+  }
+  movingMode = 3;
+
+  if(millis() - startTime > maxSameMoveDelay)
+  {
+        goBackAndTurn();
+  }
+  else {
+    rouedroite.writeMicroseconds(1475);   
+    rouegauche.writeMicroseconds(1700);
+  }
+
 }
 
 //One wheel stoped
 void turnLeft()
 {
-  rouedroite.writeMicroseconds(1300);   
-  rouegauche.writeMicroseconds(1525);
+
+    if(movingMode != 4)
+  {
+    startTime = millis();
+  }
+  movingMode = 4;
+
+  if(millis() - startTime > maxSameMoveDelay)
+  {
+        goBackAndTurn();
+  }
+  else {
+    rouedroite.writeMicroseconds(1300);   
+    rouegauche.writeMicroseconds(1525);
+  }
+
+
 }
 
 //turn on it self
 void turnRightSelf()
 {
+  movingMode = -1;
   rouedroite.writeMicroseconds(1600);   
   rouegauche.writeMicroseconds(1600);
   delay(400);
   stop();
-  delay(125);
+  delay(50);
 }
 
 //turn on it self
 void turnLeftSelf()
 {
-  rouedroite.writeMicroseconds(1400);   
-  rouegauche.writeMicroseconds(1400);
-  delay(400);
+  movingMode = -1;
+  rouedroite.writeMicroseconds(1450);   
+  rouegauche.writeMicroseconds(1450);
+  delay(584);
   stop();
-  delay(125);
+  delay(50);
 
+}
+
+void goBackAndTurn() {
+  movingMode = 2;
+  rouedroite.writeMicroseconds(1530);   
+  rouegauche.writeMicroseconds(1400);
+  delay(300);
 }
 
 void stop()
 {
+  movingMode = 0;
   rouedroite.writeMicroseconds(1500);   
   rouegauche.writeMicroseconds(1500);
 }
